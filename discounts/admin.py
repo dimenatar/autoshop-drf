70a47d82de1faed1080a7f43c74b9766a34121d3
dimenatar <dimenatar25@gmail.com>
2025-01-14 23:34:13 +0300

    squashed migrations + project clean up

--- discounts/admin.py
+++ discounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from discounts.models import GeneralDiscount, UserPersonalDiscount, CarDiscount
+from discounts.models import GeneralDiscount, UserPersonalDiscount, CarDiscount, AutoShopPersonalDiscount
 
 admin.site.register(GeneralDiscount)
 admin.site.register(CarDiscount)
 admin.site.register(UserPersonalDiscount)
+admin.site.register(AutoShopPersonalDiscount)
